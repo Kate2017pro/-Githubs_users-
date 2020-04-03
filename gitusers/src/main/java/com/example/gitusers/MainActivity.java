3a96e5dee3331e869356b54458a229d2ca65d974
Kate2017pro <yastepkat2007@yandex.ru>
2020-04-03 15:35:23 +0300

    Delete the list

--- gitusers/src/main/java/com/example/gitusers/MainActivity.java
+++ gitusers/src/main/java/com/example/gitusers/MainActivity.java
@@ -31,14 +31,11 @@ import javax.net.ssl.HttpsURLConnection;
 
 import static java.security.AccessController.getContext;
 
-public class MainActivity extends AppCompatActivity implements OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "myLogs";
 
-    Button btn_det;
-    TextView inf_user;
-    // Handler handler;
-    List<String> users1 = new ArrayList<>();
+   // List<String> users1 = new ArrayList<>();
     RecyclerView recyclerView;
 
     @Override
@@ -47,12 +44,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "найдем View-элементы");
 
-     //   btn_det = findViewById(R.id.detail1);
-     //   inf_user = findViewById(R.id.inf_users1);
-     //   btn_det.setOnClickListener(this);
-
         recyclerView=findViewById(R.id.recycle_view);
-
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         new GetListUsers().execute("https://api.github.com/users");
@@ -67,7 +59,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
     }
 
     class GetListUsers extends AsyncTask<String, Void, Void> {
-        List<String> users = new ArrayList<>();
+     //   List<String> users = new ArrayList<>();
         List<UsersClass> userclasslist = new ArrayList<>();
 
         protected Void doInBackground(String... urls) {
@@ -103,7 +95,8 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
                     JsonReader jsonReader = new JsonReader(responseBodyReader);
                     jsonReader.beginArray();
                     while (jsonReader.hasNext()) {
-                        users.add(readMessage(jsonReader));
+                      //  users.add(readMessage(jsonReader));
+                        userclasslist.add(readMessage(jsonReader));
                     }
                     jsonReader.endArray();
                   /*  Message msg = new Message();
@@ -128,11 +121,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         protected void onPostExecute(Void avoid) {
             super.onPostExecute(avoid);
 
-            //TextView inf_user;
-            // inf_user = findViewById(R.id.inf_users1);
-        //    inf_user.setText(users.get(0));
-          //  inf_user.setText(userclasslist.get(1).getId());
-            users1 = users;
+          //  users1 = users;
 
             final DataAdapter dataAdapter = new DataAdapter(userclasslist, MainActivity.this);
             recyclerView.setAdapter(dataAdapter);
@@ -141,7 +130,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
 
         }
 
-        private String readMessage(JsonReader reader) throws IOException {
+        private UsersClass readMessage(JsonReader reader) throws IOException {
 
             String text = null;
          //   UsersClass user;
@@ -165,8 +154,9 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
       /*  Message mg=new Message();
         mg.obj=text;
         return mg;*/
-      userclasslist.add(new UsersClass(text,id));
-            return text;
+     // userclasslist.add(new UsersClass(text,id));
+          //  return text;
+            return new UsersClass(text,id);
         }
     }
 
@@ -185,7 +175,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
                 this.finish();
                 break;
             case R.id.menu_about:
-                Log.d(TAG, "Перключение экрана");
+                Log.d(TAG, "Переключение экрана");
                 Intent intent = new Intent(this, About_Activiti.class);
                 startActivity(intent);
                 break;
@@ -198,16 +188,4 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-        /*   case R.id.detail1:
-
-                // inf_user.setText(users.get(3));
-                Intent intent = new Intent(this, Detail_information.class);
-                intent.putExtra("name_user", users1.get(1));
-                startActivity(intent);
-                break;*/
-        }
-    }
 }
